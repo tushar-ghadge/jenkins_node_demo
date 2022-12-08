1ba36333af52be557058a7d8311f20592ddbdf27
Tushar Ghadge <tushar-ghadge@users.noreply.github.com>
2022-12-08 22:51:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-ssh -i ~/keys/.ssh/id_rsa root@139.59.6.53
-ll
-whoami
-git pull origin master
-npm install --production
-pm2 restart all
+ssh -i ~/keys/.ssh/id_rsa root@139.59.6.53 'cd ~/jenkins_node_demo/ && git pull && npm install --production && pm2 start app.js --name demo"
 exit
